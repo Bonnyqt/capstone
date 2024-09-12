69e15c8de9c99b818ff38600353c16ad12b01f45
Bonnyqt <marquezjonbon@gmail.com>
2024-09-12 08:20:15 +0800

    fifth commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User  # If using Django's built-in User model
 from django.conf import settings
+from django.contrib.auth.hashers import make_password, check_password
+from django.utils import timezone
 
 class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)  
 
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # Only hash password when creating a new user
+            self.password = make_password(self.password)
+        super(User, self).save(*args, **kwargs)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+    
 
 class Feedback(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     feedback = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now) 
 
     def __str__(self):
         return f"Feedback from {self.email or 'Anonymous'}"
+    
+    

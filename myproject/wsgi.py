2ba97b3ae0d49f94fd617a2767eef88d6a94b8d2
Bonnyqt <160300323+Bonnyqt@users.noreply.github.com>
2024-11-23 18:49:13 +0800

    Update wsgi.py

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,20 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'myproject' project.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-# Create the default WSGI application
 application = get_wsgi_application()
-
-# Add WhiteNoise to serve static files
 application = WhiteNoise(application)
-
-# Add custom static files directories if needed
-application.add_files("/capstone/myapp/static/", prefix="more-files/")
-
-# Optionally, you can also set the root directory where static files are served
-application.root = "staticfiles"  # You can modify this as needed
+application.add_files(BASE_DIR / 'static', prefix='more-files/')

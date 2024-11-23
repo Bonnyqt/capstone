c1ccf619e422e1dee08bbc9350758b426ec33a91
Bonnyqt <160300323+Bonnyqt@users.noreply.github.com>
2024-11-23 18:52:07 +0800

    Update wsgi.py

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,11 +8,17 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+# Define BASE_DIR manually
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
+
+# Add custom static files directories
 application.add_files(BASE_DIR / 'static', prefix='more-files/')

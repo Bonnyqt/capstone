a3e4eb7863553f19fd50feb0cbda967091da0f6f
Bonnyqt <160300323+Bonnyqt@users.noreply.github.com>
2024-11-23 17:44:24 +0800

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('')
-DEBUG = os.environ.get('DEBUG', 'False').lower = 'true'
+DEBUG = False
 # SECURITY WARNING: don't run with debug turned on in production!
 
 

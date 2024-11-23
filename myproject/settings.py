a4095b4f2fd1540e5c818f9e39a07dad559ebae8
Bonnyqt <160300323+Bonnyqt@users.noreply.github.com>
2024-11-23 18:32:17 +0800

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,9 +30,9 @@ PASSWORD_HASHERS = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STORAGES = {
     # ...
     "staticfiles": {
